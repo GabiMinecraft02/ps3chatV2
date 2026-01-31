3ab7437ae8f863795d748e02aec13403b4e26f55
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-01-31 11:47:07 +0100

    Update vocal.js

--- static/js/vocal.js
+++ static/js/vocal.js
@@ -1,5 +1,9 @@
 const localAudio = document.getElementById("localAudio");
 navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
     localAudio.srcObject = stream;
-    // Ici: créer un Peer pour chaque personne connectée
+    // Ici tu peux créer un Peer avec simple-peer ou adapter à Supabase pour signaling
+});
+
+document.getElementById("mute-btn").addEventListener("click", () => {
+    localAudio.srcObject.getAudioTracks()[0].enabled = !localAudio.srcObject.getAudioTracks()[0].enabled;
 });
